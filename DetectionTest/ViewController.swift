80770d7d54d1af5d44ff01bd5deee41691fed805
komplexon3 <m@marcs-macbook-pro.home>
2017-03-21 20:51:26 +0100

    Collection of research for simple test - UI missing - detImage -> imageDisplayed missing

--- DetectionTest/ViewController.swift
+++ DetectionTest/ViewController.swift
@@ -8,18 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,
+    UIImagePickerControllerDelegate,
+UINavigationControllerDelegate  {
+    
+    var detector: CIDetector?
+    var detImage: CIImage?
+    
+    @IBOutlet weak var imageDisplayed: UIImageView!
+    
+    @IBAction func openCameraButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func performDetection(sender: AnyObject) {
+        if detImage != nil {
+            detector = prepareRectangleDetector()
+            detImage = performRectangleDetection(detImage!)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        imageDisplayed.image = image
+        detImage = CIImage(image: image)
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    func prepareRectangleDetector() -> CIDetector {
+        let options: [String: Any] = [CIDetectorAccuracy: CIDetectorAccuracyHigh, CIDetectorAspectRatio: 1.0]
+        return CIDetector(ofType: CIDetectorTypeRectangle, context: nil, options: options)!
+    }
+    
+    func performRectangleDetection(_ image: CIImage) -> CIImage? {
+        var resultImage: CIImage?
+        if let detector = detector {
+            // Get the detections
+            let features = detector.features(in: image)
+            for feature in features as! [CIRectangleFeature] {
+                resultImage = drawHighlightOverlayForPoints(image, topLeft: feature.topLeft, topRight: feature.topRight,
+                                                            bottomLeft: feature.bottomLeft, bottomRight: feature.bottomRight)
+            }
+        }
+        return resultImage
+    }
+    
+    func drawHighlightOverlayForPoints(_ image: CIImage, topLeft: CGPoint, topRight: CGPoint,
+                                       bottomLeft: CGPoint, bottomRight: CGPoint) -> CIImage {
+        var overlay = CIImage(color: CIColor(red: 1.0, green: 0, blue: 0, alpha: 0.5))
+        overlay = overlay.cropping(to: image.extent)
+        overlay = overlay.applyingFilter("CIPerspectiveTransformWithExtent",
+                                         withInputParameters: [
+                                            "inputExtent": CIVector(cgRect: image.extent),
+                                            "inputTopLeft": CIVector(cgPoint: topLeft),
+                                            "inputTopRight": CIVector(cgPoint: topRight),
+                                            "inputBottomLeft": CIVector(cgPoint: bottomLeft),
+                                            "inputBottomRight": CIVector(cgPoint: bottomRight)
+            ])
+        return overlay.compositingOverImage(image)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
